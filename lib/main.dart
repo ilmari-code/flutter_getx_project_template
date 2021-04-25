95f135615bcf2f4f40de12d9bca51871384216db
jiayuanfang <214681658@qq.com>
2021-04-25 10:25:58 +0800

    修改Dart sdk版本为2.12，使用空安全

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,13 @@
+/*
+ * @Description: 
+ * @Autor: ilmari
+ * @Date: 2021-04-19 16:10:54
+ * @LastEditors: ilmari
+ * @LastEditTime: 2021-04-25 10:23:58
+ */
 import 'package:flutter/material.dart';
+import 'package:flutter_getx_project_template/app/lang/translation_service.dart';
+import 'package:flutter_getx_project_template/app/shared/logger/logger_utils.dart';
 
 import 'package:get/get.dart';
 
@@ -15,8 +24,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       title: "Application",
+      debugShowCheckedModeBanner: false,
+      enableLog: true,
       initialRoute: AppPages.INITIAL,
       getPages: AppPages.routes,
+      logWriterCallback: Logger.write,
+      locale: TranslationService.locale,
+      fallbackLocale: TranslationService.fallbackLocale,
+      translations: TranslationService(),
     );
   }
 }
