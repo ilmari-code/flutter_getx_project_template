222947ed6f30cdb7ae67adc46b32f6af9e16390f
jiayuanfang <214681658@qq.com>
2021-05-13 13:56:04 +0800

    fixed bug

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -2,7 +2,7 @@
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=/Applications/flutter"
 export "FLUTTER_APPLICATION_PATH=/Users/zangyingzhen/Documents/other/flutter_getx_project_template"
-export "FLUTTER_TARGET=/Users/zangyingzhen/Documents/other/flutter_getx_project_template/lib/main_dev.dart"
+export "FLUTTER_TARGET=/Users/zangyingzhen/Documents/other/flutter_getx_project_template/lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "FLUTTER_BUILD_NAME=1.0.0"
--- lib/main.dart
+++ lib/main.dart
@@ -3,26 +3,13 @@
  * @Autor: ilmari
  * @Date: 2021-04-19 16:10:54
  * @LastEditors: ilmari
- * @LastEditTime: 2021-05-12 16:25:29
+ * @LastEditTime: 2021-05-13 08:26:41
  */
 import 'package:flutter/material.dart';
-import 'package:flutter_getx_project_template/app/utils/sp_utils/sp_utils.dart';
 import 'package:flutter_getx_project_template/my_app.dart';
-import 'package:statsfl/statsfl.dart';
-
 import 'app/plugins/options/options.dart';
 
 void main() async {
   await initApp(Environment.prod);
-  runApp(StatsFl(
-    isEnabled: true, //Toggle on/off
-    width: 80, //Set size
-    height: 30, //
-    maxFps: 90, // Support custom FPS target (default is 60)
-    // showText: true, // Hide text label
-    sampleTime: .5, //Interval between fps calculations, in seconds.
-    totalTime: 15, //Total length of timeline, in seconds.
-    align: Alignment.centerRight,
-    child: MyApp(),
-  ));
+  runApp(MyApp());
 }
